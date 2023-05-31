f099d673aa3f32fcfcc19e4ef1c4f84629da1d58
Josvany_G <josvanyg@gmail.com>
2023-05-31 12:17:36 -0600

    se habilita los cors que permite consumo de app desde front end

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using PizzaStore.Context;
 using PizzaStore.Models;
 
+const string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
@@ -12,22 +14,34 @@ builder.Services.AddSqlite<PizzaDb>(connectionString);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
    {
-       c.SwaggerDoc("v1", new OpenApiInfo {
-         Title = "PizzaStore API",
-         Description = "Making the Pizzas you love",
-         Version = "v1" });
+       c.SwaggerDoc("v1", new OpenApiInfo
+       {
+           Title = "PizzaStore API",
+           Description = "Making the Pizzas you love",
+           Version = "v1"
+       });
    });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+      builder =>
+      {
+          builder.WithOrigins("*");
+      });
+});
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI(c =>
    {
-     c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
-     c.RoutePrefix = string.Empty; // esto se usa para no tener que colocar en la url swagger
+       c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+       c.RoutePrefix = string.Empty; // esto se usa para no tener que colocar en la url swagger
    });
 
+//activate cors
+app.UseCors(MyAllowSpecificOrigins);
 
 //Get all
 app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
@@ -46,25 +60,25 @@ app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsyn
 //update element
 app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatepizza, int id) =>
 {
-      var pizza = await db.Pizzas.FindAsync(id);
-      if (pizza is null) return Results.NotFound();
-      pizza.Name = updatepizza.Name;
-      pizza.Description = updatepizza.Description;
-      await db.SaveChangesAsync();
-      return Results.NoContent();
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+    pizza.Name = updatepizza.Name;
+    pizza.Description = updatepizza.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
 });
 
 //Delete element
 app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) =>
 {
-   var pizza = await db.Pizzas.FindAsync(id);
-   if (pizza is null)
-   {
-      return Results.NotFound();
-   }
-   db.Pizzas.Remove(pizza);
-   await db.SaveChangesAsync();
-   return Results.Ok();
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null)
+    {
+        return Results.NotFound();
+    }
+    db.Pizzas.Remove(pizza);
+    await db.SaveChangesAsync();
+    return Results.Ok();
 });
 
 
