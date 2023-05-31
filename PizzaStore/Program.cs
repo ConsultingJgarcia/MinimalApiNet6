04178161d56a3b1404c2e7338ae91e962cbfaac9
Josvany_G <josvanyg@gmail.com>
2023-05-31 10:58:23 -0600

    cambio de datos estaticos por datatos en memoria ef core

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -1,7 +1,12 @@
 using Microsoft.OpenApi.Models;
-using PizzaStore.DB;
+
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Context;
+using PizzaStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -22,11 +27,22 @@ app.UseSwaggerUI(c =>
      c.RoutePrefix = string.Empty; // esto se usa para no tener que colocar en la url swagger
    });
 
-app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
-app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
-app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
-app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
-app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
+
+
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+
+app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
+
+// app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+
+// app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+// app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+// app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
 
 
 app.Run();
